11dada30f60a77901474bbd079e0023a1a4efddf
Mikhail Apanasenko <43650260+krasmikes@users.noreply.github.com>
2022-07-14 15:39:31 +0400

    Completed first task

---
+++ IOSINT-16/StorageService/StorageService.h
@@ -0,0 +1,18 @@
+//
+//  StorageService.h
+//  StorageService
+//
+//  Created by Михаил Апанасенко on 14.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageService.
+FOUNDATION_EXPORT double StorageServiceVersionNumber;
+
+//! Project version string for StorageService.
+FOUNDATION_EXPORT const unsigned char StorageServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageService/PublicHeader.h>
+
+
