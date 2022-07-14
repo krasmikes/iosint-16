3265190f9cde8ded0fdc6d201e1708fc9d67591f
Mikhail Apanasenko <43650260+krasmikes@users.noreply.github.com>
2022-07-14 14:16:01 +0400

    Project creation

---
+++ IOSINT-16/IOSINT-16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSINT-16
+//
+//  Created by Михаил Апанасенко on 14.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
